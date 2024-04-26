f84eac69d93fd4362bf30a8f5e66817d23998b96
ambolling <87670660+ambolling@users.noreply.github.com>
2024-04-26 13:28:04 -0400

    Add files via upload

---
+++ rmsd_generator.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Input file containing URLs
+input_file="isoforms.txt"
+# Store all isoforms in array file names to be accessed for subsiquent use
+file_names=()
+
+# Read each line from the file and extract the value after the last equal sign to represent protein name
+while IFS= read -r url; do
+    # Extract the name of the protein from the URL
+    URL="$url"
+    name=$(echo "$url" | grep -oE '[^=]+$')
+    file_names+=("$name")
+done < "$input_file"
+
+
+for file1 in "${file_names[@]}"; do
+    echo "$file1"
+    for file2 in "${file_names[@]}"; do
+        echo "$file2"
+        if [ "$file1" != "$file2" ]
+        then
+            combined="$file1$file2"
+            if ! in_array $combined ${combinations[@]}; then
+                echo Running rmsd analysis on "$file1" and "$file2"
+                python3 rmsd_plot.py "$file1".pdb "$file2".pdb
+            fi  
+        fi
+    done
+done
