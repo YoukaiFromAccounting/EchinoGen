0f8f79f6a5e47d5c260fb9591eb3a37c008cdc11
ambolling <87670660+ambolling@users.noreply.github.com>
2024-04-26 13:29:22 -0400

    Update rmsd_generator.sh

--- rmsd_generator.sh
+++ rmsd_generator.sh
@@ -20,11 +20,8 @@ for file1 in "${file_names[@]}"; do
         echo "$file2"
         if [ "$file1" != "$file2" ]
         then
-            combined="$file1$file2"
-            if ! in_array $combined ${combinations[@]}; then
-                echo Running rmsd analysis on "$file1" and "$file2"
-                python3 rmsd_plot.py "$file1".pdb "$file2".pdb
-            fi  
+            echo Running rmsd analysis on "$file1" and "$file2"
+            python3 rmsd_plot.py "$file1".pdb "$file2".pdb  
         fi
     done
 done
